1258439fb785b9bbf8c35572c85cd71171e112ab
Ronaldo <ronaldo.vieira2505@gmail.com>
2025-09-18 11:28:19 -0300

    fix(users): Corrige importacao de views em urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import login_view
+from .import views
 
 urlpatterns = [
-    path('api/login', login_view, name='api_login'),
+    path('api/login', views.login_view, name='api_login'),
 ]
