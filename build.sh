832b1d9958ebf5ca3042123d365dae7e0ce75859
Ronaldo <ronaldo.vieira2505@gmail.com>
2025-11-24 10:25:53 -0300

    chore(infra): Prepara configuracoes para Deploy (Gunicorn/Whitenoise)

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Sai se der erro
+set -o errexit
+
+# Instala as bibliotecas
+pip install -r requirements.txt
+
+# Prepara o CSS para produção
+python manage.py collectstatic --no-input
+
+# Aplica migrações no banco de dados
+python manage.py migrate
